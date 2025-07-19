2112983a2a9b891e54176baf3605cf92f22b9e1d
Simmi <simmi.chaudhary11@gmail.com>
2025-07-19 18:06:03 +0530

    Setter Injection

--- src/main/java/org/hibpro/Alien.java
+++ src/main/java/org/hibpro/Alien.java
@@ -1,7 +1,29 @@
 package org.hibpro;
 
 public class Alien {
+    private int age;
+    private Laptop lap;
+    public Alien(){
+        System.out.println("Alien");
+    }
+    public int getAge() {
+        return age;
+    }
+    public void setAge(int age) {
+        System.out.println("setter called");
+        this.age = age;
+    }
+
+    public Laptop getLap() {
+        return lap;
+    }
+
+    public void setLap(Laptop lap) {
+        this.lap = lap;
+    }
+
     public void code(){
-        System.out.println("Voila!, Alien is coding");
+        System.out.println("code");
+        lap.compile();
     }
 }
---
+++ src/main/java/org/hibpro/Laptop.java
@@ -0,0 +1,10 @@
+package org.hibpro;
+
+public class Laptop {
+    public Laptop(){
+        System.out.println("Laptop Constructor");
+    }
+    public void compile(){
+        System.out.println("Laptop compile");
+    }
+}
